9b15c54eb153b30bafad910e392a37178517f19b
EviZa96 <161627325+EviZa96@users.noreply.github.com>
2025-02-19 10:37:16 +0200

    debug form

--- mail.php
+++ mail.php
@@ -46,7 +46,7 @@ use PHPMailer\PHPMailer\SMTP;
 
 $mail = new PHPMailer(true);
 
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
